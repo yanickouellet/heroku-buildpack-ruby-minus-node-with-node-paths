99d7c4cbd2bd4fcccf18455bc32dcf173d8bf916
mattwalters <matthewrobertwalters@gmail.com>
2014-04-03 17:47:03 -0700

    patching buildpack to never install node binaries

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -683,7 +683,12 @@ params = CGI.parse(uri.query || "")
   # @note execjs will blow up if no JS RUNTIME is detected and is loaded.
   # @return [Array] the node.js binary path if we need it or an empty Array
   def add_node_js_binary
-    bundler.has_gem?('execjs') ? [NODE_JS_BINARY_PATH] : []
+    # bundler.has_gem?('execjs') ? [NODE_JS_BINARY_PATH] : []
+    # 
+    # we never want this buildpack to install nodejs binaries
+    # instead we are using ddollar's multibuildpack and 
+    # including a standalone node buildpack
+    [ ]
   end
 
   def run_assets_precompile_rake_task
